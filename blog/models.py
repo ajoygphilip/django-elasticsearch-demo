8c50e4c6251aaa9660c6b4af394ed5f6be900acc
ajoy <ajoy.gp@fayausa.com>
2024-03-17 09:58:11 +0530

    feat: add blog app

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=20)
+    job = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
